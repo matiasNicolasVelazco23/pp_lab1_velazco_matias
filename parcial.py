ac1d6c7d2715eef216ad5186cc28dd287305f3c3
matiasNicolasVelazco23 <matiasvelazco558@gmail.com>
2023-05-27 22:53:34 -0300

    16/20 faltan 5,16,17,20

--- parcial.py
+++ parcial.py
@@ -1,5 +1,15 @@
 import re
 import json
+import os
+
+def clear_console() -> None:
+    """
+    Esta función borra la pantalla de la consola en Python esperando la entrada del usuario y luego
+    llamando al comando 'cls' para borrar la pantalla.
+    """
+    _ = input('Presiona cualquier tecla para continuar.')
+    os.system('cls')
+
 
 def leer_archivo (nombre_archivo : str) -> list:
     """
@@ -12,7 +22,7 @@ def leer_archivo (nombre_archivo : str) -> list:
         Retorna la lista, que contiene una lista de diccionarios
     """
     lista= []
-    with open (nombre_archivo, "r") as archivo: 
+    with open (nombre_archivo, "r", encoding ='utf-8') as archivo:  #UTF-8 para que permita caracteres especiales
         dict = json.load (archivo)
         lista = dict["jugadores"]
     return lista
@@ -22,7 +32,7 @@ def imprimir_dato(dato : str) -> None:
        Recibe el dato para imprimirlo mediante print
 
     Args:
-        dato (_type_): _description_
+        dato (str): dato especificado
     """
     print(dato)
 
@@ -33,10 +43,10 @@ def imprimir_menu():
     "2. Seleccionar un jugador por su índice y mostrar sus estadísticas"
     "completas\n"
     "3. Guardar las estadísticas de ese jugador en un archivo CSV.\n" 
-    #"4. Buscar un jugador por su nombre y mostrar sus logros\n"
+    "4. Buscar un jugador por su nombre y mostrar sus logros\n"
     #"5. Calcular y mostrar el promedio de puntos por partido de todo el equipo"
     # " del Dream Team, ordenado por nombre de manera ascendente. \n"
-    #6.Permitir al usuario ingresar el nombre de un jugador y mostrar si ese jugador es miembro del Salón de la Fama del Baloncesto.
+    "6.Permitir al usuario ingresar el nombre de un jugador y mostrar si ese jugador es miembro del Salón de la Fama del Baloncesto.\n"
     "7. Calcular y mostrar el jugador con la mayor cantidad de rebotes totales.\n"
     "8- Calcular y mostrar el jugador con el mayor porcentaje de tiros de campo.\n"
     "9- Calcular y mostrar el jugador con la mayor cantidad de asistencias totales.\n"
@@ -75,7 +85,7 @@ def mostrar_lista_jugadores(lista_jugadores:list, formato:bool)-> None:
         En cada bucle del for accede  a cada diccionario individual 
         de los jugadores mediante el índice, toma el valor de las claves 
         nombre y  posición de dicho índice, para luego imprimirlo por pantalla
-        con el formato indicado, agregué un espacio después del último índice.
+        con el formato indicado necesario (punto 1 y 2)
 
     Args:
         lista_jugadores (list): el parámetro que recibe representa una lista 
@@ -99,14 +109,13 @@ def mostrar_lista_jugadores(lista_jugadores:list, formato:bool)-> None:
         mensaje= ("{0}{1}{2}".format(decoracion, lista_jugadores[indice]["nombre"],
                                      posicion_si_no))
         print(mensaje)
-    print("")
 
 def es_entero(string)-> bool:
     """_summary_
         Funcion que verifica si el string es entero o no mediante RegEX
-        Verifica si la expresión es verdadera o falsa, mediante re.search
-        la expresión quiere decir, empieza con un número entero en un rango de 
-        0 al 9, una o más ocurrencias, y termina con un número entero.
+        Verifica si la expresión es verdadera o falsa, utilizando re.search
+        la expresión quiere decir.. "empieza con un número entero en un rango de 
+        0 al 9, una o más ocurrencias, y termina con un número entero".
     Args:
         string (str): se envia un string por parámetro
 
@@ -122,11 +131,13 @@ def es_entero(string)-> bool:
     else:
         return False
     
-def es_flotante(string):
+def es_flotante(string:str)->bool:
     """Función que verifica si el string es un número flotante válido mediante RegEx.
+        le doy la opción al usuario de que ponga , o ., pero después lo formateo en la 
+        función donde llamo a esta función.
     
     Args:
-        string (str): Cadena a verificar.
+        string (str): string a verificar.
     
     Returns:
         bool: True si la cadena es un número flotante válido, False en caso contrario.
@@ -135,6 +146,51 @@ def es_flotante(string):
         return True
     else:
         return False
+    
+def es_texto(string):
+    """
+       Función que verifica si el string es un texto, devuelve, admite
+       mayúsculas y minúculas, una o más ocurrencias.
+
+    Args:
+        string (str): stringa verificar
+
+    Returns:
+        bool: True si la cadena es string válido, False en caso contrario.
+    """
+    if re.match(r"(^[a-zA-Z ]+$)", string):
+        return True
+    else:
+        return False
+    
+def es_miembro_salon(logros):
+    patron = r'\bMiembro del Salon de la Fama del Baloncesto\b'
+    if re.search(patron, logros):
+        return True
+    else:
+        return False
+    
+def hay_coincidencia_inexacta(nombre_ingresado:str, lista_jugadores:list)->list:
+    """
+       Función que verifica si hay ocurrencias con el nombre de algún jugador 
+       de la lista
+
+    Args:
+        nombre_ingresado (str): nombre ingresado, validado previamente
+        lista_jugadores (list): lista de los jugadores, con sus claves
+
+    Returns:
+        list: Retorna una lista, que incluye los valores de nombres, seguidos de sus logros, 
+        en pares de 2.
+    """
+    patron = r'({0})+'.format(nombre_ingresado.lower())
+    jugadores_coincidentes = []
+    for jugador in lista_jugadores:
+        nombre_jugador = jugador["nombre"].lower()
+        if re.search(patron, nombre_jugador):
+            jugadores_coincidentes.append(jugador["nombre"])
+            jugadores_coincidentes.append(jugador["logros"])
+    return jugadores_coincidentes
     
 def verificar_input(lista_jugadores:list) -> int:
     """
@@ -158,7 +214,7 @@ def verificar_input(lista_jugadores:list) -> int:
 
         if es_entero(ingreso_indice_texto):
             ingreso_indice_entero= int(ingreso_indice_texto)
-            if ingreso_indice_entero >= len(lista_jugadores): # Acá tengo un problema, cuando pongo 12 me salta error
+            if ingreso_indice_entero >= len(lista_jugadores): 
                 print("Asegurate de no ingresar un índice que supere el tamaño de la lista, el tamaño es: {0} índices, del 0 al 11.\n"
                                                                      .format(len(lista_jugadores))
                      )
@@ -186,7 +242,7 @@ def iterar_estadisticas(lista_jugadores: list, indice_validado: int) -> str:
         mensaje += datos
     return mensaje
     
-def selecciona_jugador_por_indice(lista_jugadores: list)-> dict:
+def selecciona_jugador_por_indice(lista_jugadores: list)-> int:
     """_summary_
         Función que  selecciona un jugador mediante el índice ingresado,
         usa las funcionaes verificar_input y iterar_estadisticas
@@ -226,15 +282,40 @@ def generar_csv_estadisticas_jugador(nombre_archivo: str, lista_jugadores: list,
     Returns:
         La función no retorna nada
     """
-    with open(nombre_archivo, "w") as archivo: 
-            for clave, valor in lista_jugadores[indice_seleccionado].items():
-                if clave == "estadisticas":
-                    archivo.write(iterar_estadisticas(lista_jugadores, indice_seleccionado))
-                    break
-                mensaje = "{0}: {1}\n".format(clave.capitalize(),valor)
-                archivo.write(mensaje)
-    
+    with open(nombre_archivo, "w") as archivo:
+        lista_de_claves = []
+        lista_de_valores = []
+        for clave in lista_jugadores[indice_seleccionado]:
+            if clave != "estadisticas":
+                mensaje = "{0}".format(clave)
+                lista_de_claves.append(mensaje)
+            else:
+                for clave_estadisticas in lista_jugadores[indice_seleccionado]["estadisticas"]:
+                    mensaje_estadisticas = "{0}".format(clave_estadisticas)
+                    lista_de_claves.append(mensaje_estadisticas)
+        separador = ","
+        separado_con_coma_claves= separador.join(lista_de_claves[:-1])
+        claves=separado_con_coma_claves
 
+
+        for clave in lista_jugadores[indice_seleccionado]:
+            if clave != "estadisticas":
+                valor = str(lista_jugadores[indice_seleccionado][clave])
+                lista_de_valores.append(valor)
+            else:
+                for clave_estadisticas in lista_jugadores[indice_seleccionado]["estadisticas"]:
+                    valor_estadisticas = str(lista_jugadores[indice_seleccionado]["estadisticas"][clave_estadisticas])
+                    lista_de_valores.append(valor_estadisticas)
+        separador = ","
+        separado_con_coma_valores = separador.join(lista_de_valores[:-1])
+        valores=separado_con_coma_valores
+
+        archivo.write("{0}\n{1}".format(claves,valores))
+
+                
+            
+            
+    
 #----------------------------------------------------------------------------
 # 4)Permitir al usuario buscar un jugador por su nombre y mostrar sus logros, 
 # como campeonatos de la NBA, participaciones en el All-Star y pertenencia al Salón
@@ -267,6 +348,8 @@ def calcular_y_mostrar_dato(lista_jugadores:list,dato:str):
 
     print("El jugador con el máximo de '{0}' es {1} con un total de {2}.\n".format(dato.replace("_", " "),cadena_maximos,maximo))
 
+
+    
 def buscar_jugador_dato(lista_jugadores, tipo_valor, dato):
     respuesta_valida = False
     if tipo_valor == "valor_flotante":
@@ -283,9 +366,39 @@ def buscar_jugador_dato(lista_jugadores, tipo_valor, dato):
                 ingresar_flotante_float= float(ingresar_flotante_str)
                 respuesta_valida = True
 
-
-
+            else:
+                print("Número inválido")
         encontrar_jugadores_superiores(lista_jugadores, dato, ingresar_flotante_float)
+
+    if tipo_valor == "nombre":
+         while (respuesta_valida == False):
+            ingresar_nombre= input("Ingrese un nombre: ")
+
+            if es_texto(ingresar_nombre):
+                
+                lista_logros_jugador= hay_coincidencia_inexacta(ingresar_nombre,lista_jugadores)
+                if len(lista_logros_jugador) > 0:
+                    texto_logros_nombre_acumulado = ""
+                    for indice in range(len(lista_logros_jugador)):
+                        if indice % 2 !=0:
+                            
+                            separador_logros = " | "
+                            logros = "{0}, logros: \n{1}.\n".format(lista_logros_jugador[indice-1],separador_logros.join(lista_logros_jugador[indice]))
+                            
+                            texto_logros_nombre_acumulado += logros
+                    return texto_logros_nombre_acumulado
+  
+                else:
+                    print("El texto ingresado no coincide con ningún jugador")
+            
+            else: 
+                print("Nombre inválido")
+
+    
+
+
+
+        
 
 
 
@@ -309,7 +422,7 @@ def encontrar_jugadores_superiores(lista_jugadores, dato, maximo):
     if ningun_superior == False:
         print("Los jugadores que han superado el valor {0} de {1} son: {2}.\n".format(maximo, dato.replace("_", " "),cadena_maximos))
     else:
-        print("No hay ningún jugador que supere este valor\n")
+        print("No hay ningún jugador que supere este valor.\n")
 
 def menu_de_opciones(lista_jugadores:list):
     flag_se_selecciono = False
@@ -327,11 +440,19 @@ def menu_de_opciones(lista_jugadores:list):
                 else:
                     print("Aún no seleccionaste un índice, vuelve a intentarlo.\n")
             elif opcion == "4":
-                busca_jugador_dato(lista_jugadores, "string")
+                nombre_logros=buscar_jugador_dato(lista_jugadores, "nombre", "logros")
+                print(nombre_logros)
             elif opcion == "5":
                 calcular_y_mostrar_dato(lista_jugadores, "promedio_puntos_por_partido")
             elif opcion == "6":
-                busca_jugador_dato(lista_jugadores, "string")
+                miembro_salon=buscar_jugador_dato(lista_jugadores, "nombre", "logros")
+                miembro_salon_sanitizar=miembro_salon.replace("\n", "").replace(".", ",").replace("Miembro del Salon de la Fama del Baloncesto Universitario", "dato-erroreo").split(",")
+                miembro_salon_sanitizar_menos_ultimo= miembro_salon_sanitizar[0:-1]
+                for indice in range(len(miembro_salon_sanitizar_menos_ultimo)):
+                    if indice % 2 != 0:
+                        if es_miembro_salon(miembro_salon_sanitizar_menos_ultimo[indice]):
+                            es_miembro = "{0} pertenece al salón de la fama del baloncesto.\n".format(miembro_salon_sanitizar_menos_ultimo[indice-1])
+                            print(es_miembro)
             elif opcion == "7":
                 calcular_y_mostrar_dato(lista_jugadores, "rebotes_totales")
                 pass
@@ -370,6 +491,7 @@ def menu_de_opciones(lista_jugadores:list):
             
             else:
                 print("Opcion inválida")
+            clear_console()
 
 
 lista_jugadores = leer_archivo("dt.json")
